1ae7f831c8bc051b699e5c3fa1ddcbd75dd4f274
DMB29 <mbilaldogan90@gmail.com>
2022-03-29 19:33:39 +0300

    ul li

--- card/src/component/item/Item.js
+++ card/src/component/item/Item.js
@@ -1,21 +1,34 @@
-import React  from 'react';
+import React , {useState} from 'react';
 
 
 import "./Item.css"
 
 const Item = ({cardItem}) => {
 
-
-
+const [show,setShow] = useState(true)
 
   return (
-    <div className="item-container">
+
+    <div className="item-container" onClick= {()=> setShow(!show)}>
+        {show ? (
+        <div>
         <img className="item-img" src={cardItem.img} alt="" />
         <h3 className="item-title">{cardItem.name}</h3>
+        </div> )
+        :(
 
-
-
+    <ul className="list">
+        {
+            cardItem.options.map((option,index)=> {
+                return(
+                    <li key={index} > {option}</li>
+                )
+            })
+        }
+    </ul>)
+         }
     </div>
+
   )
 }
 
