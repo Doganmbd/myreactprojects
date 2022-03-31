723cecc1815f5c8180ed86cf9e8782b86afbd8df
DMB29 <mbilaldogan90@gmail.com>
2022-03-31 20:38:08 +0300

    finish

--- user-axios/src/Users.jsx
+++ user-axios/src/Users.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React , {useState,useEffect} from 'react' ;
+import axios  from  "axios";
+
 
 const Users = () => {
+
+    const[user,setUser] = useState([]);
+
+// sayfa ilk render olduğunda user ları direk çekmek için.
+
+useEffect (()=> {
+    axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then(res => setUser(res.data))
+    .catch(err => console.log(err))
+
+},[])
+
+
+/*     const getUser = ()=> {
+        axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        // .then(res => console.log(res))
+        .then(res => setUser(res.data))
+        .catch(err => console.log(err))
+
+    } */
   return (
     <div className="users">
         <h2>users</h2>
+            {/* <button onClick={getUser}>Get users</button> */}
+        <div className="users-cards" >
+            {user.map((item) => {
+                return (
+                    <div className="useritem" key ={item.id}>
+                        <img className="image" src={`https://avatars.dicebear.com/v2/avataaars/${item.id}.svg`} alt="" />
+                        {item.name}
+                    </div>
+                );
+            } )} 
+        </div>
 
     </div>
   )
 }
 
-export default Users
+export default Users;
